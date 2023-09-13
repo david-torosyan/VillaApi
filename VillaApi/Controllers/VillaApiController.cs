69786360c69e7ad85dc12ee465a8429ad3eb60b1
Torosyan David <davit.torosyan.0214@gmail.com>
2023-09-13 20:42:10 +0400

    Add project files.

---
+++ VillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaApi.Models;
+
+namespace VillaApi.Controllers
+{
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaModel> GetVillas()
+        {
+            return new List<VillaModel>
+            {
+                new VillaModel() {Id = 1, Name = "Pool View" },
+                new VillaModel() {Id = 2, Name = "Beach View" },
+            };
+        }
+    }
+}
---
+++ VillaApi/Models/VillaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VillaApi.Models
+{
+    public class VillaModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
