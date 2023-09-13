1ff4b9653bb9291f1dea6c15eba887aa846d8ec5
Torosyan David <davit.torosyan.0214@gmail.com>
2023-09-13 21:14:28 +0400

    Api project Added create and get

--- VillaApi/Controllers/VillaApiController.cs
+++ VillaApi/Controllers/VillaApiController.cs
@@ -1,20 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using VillaApi.Data;
 using VillaApi.Models;
 
 namespace VillaApi.Controllers
 {
-    [Route("api/VillaApi")]
+    [Route("api/[controller]")]
     [ApiController]
     public class VillaApiController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaModel> GetVillas()
+        public ActionResult<IEnumerable<VillaModel>> GetVillas()
         {
-            return new List<VillaModel>
-            {
-                new VillaModel() {Id = 1, Name = "Pool View" },
-                new VillaModel() {Id = 2, Name = "Beach View" },
-            };
+            return Ok(VillaData.Villas);
         }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        public ActionResult<VillaModel> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaData.Villas.Where(u => u.Id == id).FirstOrDefault();
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        public ActionResult<VillaModel> CreateVilla([FromBody] VillaModel villa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (villa == null)
+            {
+                return BadRequest();
+            }
+            if (villa.Id > 0)
+            {
+                return BadRequest();
+
+            }
+
+            villa.Id = VillaData.Villas.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaData.Villas.Add(villa);
+
+            return CreatedAtRoute("GetVilla", new { id = villa.Id }, villa);
+        }
+
+
     }
 }
--- VillaApi/Models/VillaModel.cs
+++ VillaApi/Models/VillaModel.cs
@@ -1,8 +1,12 @@
-﻿namespace VillaApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillaApi.Models
 {
     public class VillaModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
     }
 }
